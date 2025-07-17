5e4e3b7b051c7575de65e967dfa83071014b36c8
emmanuelkawekwune <emmanuelkawekwune@punch.agency>
2025-07-17 06:40:10 +0100

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@
 import type { Metadata } from "next";
 import { DM_Sans } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 // Load DM Sans font with weights and variable
 const dmSans = DM_Sans({
@@ -12,8 +13,32 @@ const dmSans = DM_Sans({
 });
 
 export const metadata: Metadata = {
-  title: "My Portfolio",
-  description: "A modern portfolio built with Next.js and DM Sans",
+  title: "Emmanuel Kawekwune | Software Developer & Full Stack Engineer",
+  description:
+    "Showcasing the projects and skills of Emmanuel Kawekwune, a passionate software developer specializing in React.js, Next.js, Node.js, and modern web technologies. Explore portfolio, experience, and contact info.",
+  openGraph: {
+    title: "Emmanuel Kawekwune | Software Developer Portfolio",
+    description:
+      "A modern developer portfolio built with Next.js. Explore projects, skills, and professional experience of Emmanuel Kawekwune.",
+    url: "https://kawekwune-emmanuel.vercel.app",
+    siteName: "Emmanuel Kawekwune Portfolio",
+    images: [
+      {
+        url: "https://kawekwune-emmanuel.vercel.app/first.png", // <- Use your image name and URL here
+        width: 1200,
+        height: 630,
+        alt: "Emmanuel Kawekwune Portfolio",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Emmanuel Kawekwune | Software Developer Portfolio",
+    description:
+      "A modern portfolio built with Next.js, featuring the work of Emmanuel Kawekwune, a full stack web developer.",
+    images: ["https://kawekwune-emmanuel.vercel.app/first.png"], // <- Same here
+  },
 };
 
 export default function RootLayout({
@@ -31,6 +56,7 @@ export default function RootLayout({
       </head>
       <body className={`${dmSans.variable} w-full antialiased`}>
         {children}
+        <Analytics/>
       </body>
     </html>
   );
